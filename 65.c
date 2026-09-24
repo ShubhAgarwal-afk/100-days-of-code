0c1ea02bee173d12f96511aa264807c1132530f1
ShubhAgarwal-afk <shubhagarwal4934@gmail.com>
2026-09-24 18:41:34 +0530

    Uploading the code

---
+++ 65.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+   {int n;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+     arr[n];
+	 printf("Enter your elements(in sorted manner):\n");
+for(int j=0;j<n;j++)
+   { scanf("%d",&arr[j]);
+   }
+   int a;
+   printf("Enter the number you want to search:");
+   scanf("%d",&a):
+
